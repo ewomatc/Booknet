6be632cd1578b1bf0ce90919c729df9377505a29
Great Diro <iamgreatdiro@gmail.com>
2023-05-23 22:22:39 +0100

    added controller function to create and save a new genre

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -10,7 +10,7 @@ exports.genre_list = asyncHandler(async (req, res, next) => {
   res.render('genre_list', ({title: 'Genre List', genre_list: allGenre}))
 });
 
-// Display detail page for a specific Genre.
+// Display detail page for a specific Genre. 
 exports.genre_detail = asyncHandler(async (req, res, next) => {
   const [genre, booksInGenre] = await Promise.all([ Genre.findById(req.params.id).exec(), Book.find({genre: req.params.id}, 'title summary').exec() ])
 
@@ -31,9 +31,42 @@ exports.genre_create_get =  (req, res, next) => {
 };
 
 // Handle Genre create on POST.
-exports.genre_create_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Genre create POST");
-});
+exports.genre_create_post = [
+  //this post request takes in an arry that first validates and sanitizes the data entered using the imported 'body' module from express-validator
+  //.trim removes whitespaces from both ends, .isLength check to make sure it's not empty, and .escape removes html characters that may be potentially used for cross-site scripting
+  body('name', 'Genre should contain at least three characters').trim().isLength({min: 3}).escape(),
+  //process the request after validation
+  asyncHandler( async (req, res, next) => {
+    //extract the validation errors from the request 
+    const errors = validationResult(req)
+
+    //then the genre object can be created, p.s, only after validating the input like above
+    //p.s - the 'name' is gotten from the form field 'name' called 'name' which we have validated above. 
+    const genre = new Genre({ name: req.body.name })
+
+    // now using the errors we extracted with validationResult() above, check if the errors is empty. If there are errors, re-render the form, (this time it should contain the previously inputed and validated data)
+    //if there are no errors, proceed to save the newly added genre to the database.
+    if (!errors.isEmpty()) {
+      res.render('genre_form', { title: 'Create a new Genre', genre: genre, errors: errors.array() })
+    }
+    //now if the data from the form contains no errors, we have to check if there is an existing genre with the same name.
+    //if there is, we simply redirect the user to that [existing] genre's details.
+    //if the genre doesnt uet exist, we save it to the database and redirect to the genre detail of the newly created genre
+    else {
+      const genreExists = await Genre.findOne({name: req.body.name}).exec()
+
+      if(genreExists) {
+        //the url here is the the one we created as a virtual in the model definition
+        res.redirect(genreExists.url)
+      }
+      else {
+        await genre.save()
+
+        res.redirect(genre.url)
+      }
+    }
+  })
+]
 
 // Display Genre delete form on GET.
 exports.genre_delete_get = asyncHandler(async (req, res, next) => {
